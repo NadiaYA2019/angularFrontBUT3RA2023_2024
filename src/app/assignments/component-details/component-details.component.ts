acd4a67e12876e33771cca22735cd10c37c7ca7a
NadiaYA2019 <nadia.yacoubi.ayadi@gmail.com>
2023-12-02 09:39:03 +0100

    Update component-details

--- src/app/assignments/component-details/component-details.component.ts
+++ src/app/assignments/component-details/component-details.component.ts
@@ -1,34 +1,36 @@
-import { Component, EventEmitter, Output, Input, OnChanges, SimpleChanges, OnInit } from '@angular/core';
-import { Assignment } from '../assignment.model';
-import { AssignmentsService } from 'src/app/shared/assignments.service';
+import {
+  Component,
+  EventEmitter,
+  Output,
+  Input,
+  OnChanges,
+  SimpleChanges,
+  OnInit,
+} from '@angular/core';
+import { Assignment } from '../../shared/models/assignment.model';
+import { AssignmentsService } from 'src/app/shared/services/assignments.service';
 import { Subscription } from 'rxjs';
 import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-component-details',
   templateUrl: './component-details.component.html',
-  styleUrls: ['./component-details.component.css']
+  styleUrls: ['./component-details.component.css'],
 })
 export class ComponentDetailsComponent implements OnInit {
-
   constructor(
     private assignmentService: AssignmentsService,
     private activatedRoute: ActivatedRoute,
-    private router: Router) {
-
-  }
+    private router: Router
+  ) {}
   selectedAssignment: Assignment | null = null;
   subscriptions: Subscription[] = [];
   ngOnInit() {
-
     //const id = parseInt(this.activatedRoute.snapshot.params['id'], 10);
-    this.activatedRoute.paramMap.subscribe(params => {
-      const id = +params.get('id');
-      this.assignmentService.selectAssignment(id);
-
+    this.activatedRoute.paramMap.subscribe((params) => {
+      const id = params.get('id');
+      this.assignmentService.selectAssignment(id ? id : '');
     });
-
-
 
     let subscription = this.assignmentService.selectedAssignment.subscribe(
       (selectedAssignment) => (this.selectedAssignment = selectedAssignment)
@@ -36,28 +38,14 @@ export class ComponentDetailsComponent implements OnInit {
     this.subscriptions.push(subscription);
   }
 
-
-
-
   onAssignmentRendu() {
-
     if (this.selectedAssignment) {
       this.assignmentService.updateAssignment();
-      this.router.navigate(["/home"]);
-
     }
-
   }
-
-
 
   onDeleteAssignment() {
-
     if (!this.selectedAssignment) return;
     this.assignmentService.deleteAssignment();
-    this.router.navigate(["/home"]);
-
-
   }
-
 }
